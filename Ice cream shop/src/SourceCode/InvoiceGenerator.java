0d6b3c22bd3691ffcaa3ea8510d7d5cedca9db5b
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2024-11-20 15:56:04 +0600

    InvoiceGenerator class is added and main class is deleted

---
+++ Ice cream shop/src/SourceCode/InvoiceGenerator.java
@@ -0,0 +1,33 @@
+package SourceCode;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+class InvoiceGenerator {
+    public void generateInvoice(Order order) throws IOException {
+        try (FileWriter writer = new FileWriter("Invoice.txt")) {
+            writer.write("Ice Cream Shop Invoice\n");
+            List<Item> items = order.getItems();
+            items.stream()
+                    .distinct()
+                    .forEach(item -> {
+                        long count = items.stream().filter(i -> i.equals(item)).count();
+                        try {
+                            writer.write(String.format("%s - %d time(s): $%.2f\n",
+                                    item.getName(), count, count * item.getPrice()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+            if (order.isWaffleCone()) {
+                writer.write("Waffle Cone: $5.00\n");
+            }
+
+            writer.write(String.format("Subtotal: $%.2f\n", order.calculateSubtotal()));
+            writer.write(String.format("Tax: $%.2f\n", order.calculateTax()));
+            writer.write(String.format("Total Amount Due: $%.2f\n", order.calculateTotal()));
+        }
+    }
+}
--- Ice cream shop/src/SourceCode/Main.java
+++
@@ -1,7 +0,0 @@
-package SourceCode;
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-}
